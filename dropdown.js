9246006af33cda51aec82c0b4622cdc55ed9a641
CrosstalkDev <ethanmacedo586@gmail.com>
2026-01-22 23:57:12 +0000

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,4 +1,4 @@
 const profileBox = document.querySelector('.ProfileBox');
 const avatarCircle = document.querySelector('.pfp');
 
-avatarCircle.addEventListener('click', () => profileBox.classlist.toggle('show'));
+avatarCircle.addEventListener('click', () => profileBox.classList.toggle('show'));
